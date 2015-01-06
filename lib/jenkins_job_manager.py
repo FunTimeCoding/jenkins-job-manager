cec39c55b8d42b8af7b1c6941a8ad10ab78e6123
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-06 16:26:31 +0100

    updated test name, pointing out required arguments

--- lib/jenkins_job_manager.py
+++ lib/jenkins_job_manager.py
@@ -4,10 +4,11 @@ class JenkinsJobManager:
 
     def run(self):
         parser = argparse.ArgumentParser(description='Generate a config.xml for jenkins jobs.')
-        parser.add_argument('-u', '--url', help='URL to the repository to check out on Jenkins')
+        requiredNamed = parser.add_argument_group('required named arguments')
+        requiredNamed.add_argument('-u', '--url', help='URL to the repository to check out on Jenkins')
         args = parser.parse_args()
 
-        if not args.url == None:
+        if args.url is not None:
             print(args.url)
         else:
             parser.print_help()

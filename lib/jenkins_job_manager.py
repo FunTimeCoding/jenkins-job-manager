c5ec61e228efc67489a02e3c96aeb77fe4aa2c8e
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-06 04:11:26 +0100

    added argparse example

--- lib/jenkins_job_manager.py
+++ lib/jenkins_job_manager.py
@@ -1,5 +1,15 @@
+import argparse
+
 class JenkinsJobManager:
 
     def run(self):
-        print('Hello World')
+        parser = argparse.ArgumentParser(description='Generate a config.xml for jenkins jobs.')
+        parser.add_argument('-u', '--url', help='URL to the repository to check out on Jenkins')
+        args = parser.parse_args()
+
+        if not args.url == None:
+            print(args.url)
+        else:
+            parser.print_help()
+
         return 0

2226efc34b960f8374af64099db1e898bca3176d
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-08 10:46:18 +0100

    added xml generation sample code

--- lib/jenkins_job_manager.py
+++ lib/jenkins_job_manager.py
@@ -1,7 +1,8 @@
 import argparse
+from lxml import etree
+
 
 class JenkinsJobManager:
-
     def run(self):
         parser = argparse.ArgumentParser(description='Generate a config.xml for jenkins jobs.')
         requiredNamed = parser.add_argument_group('required named arguments')
@@ -10,7 +11,31 @@ class JenkinsJobManager:
 
         if args.url is not None:
             print(args.url)
+            self.create_xml()
         else:
             parser.print_help()
 
         return 0
+
+    def create_xml(self):
+        root = etree.Element("project")
+
+        actions = etree.Element("actions")
+        root.append(actions)
+
+        desc = etree.Element("description")
+        root.append(desc)
+
+        root.append(etree.Element("keepDependencies"))
+        root.append(etree.Element("properties"))
+        root.append(etree.Element("scm"))
+        root.append(etree.Element("canRoam"))
+        root.append(etree.Element("disabled"))
+        root.append(etree.Element("blockBuildWhenDownstreamBuilding"))
+        root.append(etree.Element("blockBuildWhenUpstreamBuilding"))
+        root.append(etree.Element("triggers"))
+        root.append(etree.Element("concurrentBuild"))
+        root.append(etree.Element("builders"))
+        root.append(etree.Element("publishers"))
+        root.append(etree.Element("buildWrappers"))
+        print(etree.tostring(root, pretty_print=True).decode('utf-8'))
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'http://funtimecoding.org/download/jenkins-job-manager.tar.gz',
     'author_email': 'funtimecoding@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose2'],
+    'install_requires': ['nose2', 'lxml'],
     'packages': ['NAME'],
     'scripts': [],
     'name': 'jenkins-job-manager'

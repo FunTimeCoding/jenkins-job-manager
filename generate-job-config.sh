56906fa5261edd243708ddd0ca886f9e8ac081c2
Alexander Reitzel <funtimecoding@gmail.com>
2016-08-23 17:56:38 +0200

    synchronize with python-skeleton

--- generate-job-config.sh
+++ generate-job-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 # shellcheck disable=SC2016
-jjm --locator https://github.com/FunTimeCoding/jenkins-job-manager.git --build-command 'export PATH="${HOME}/opt/python-3.5.1/bin:${PATH}"
+jjm --locator https://github.com/FunTimeCoding/jenkins-job-manager.git --build-command 'export PATH="${HOME}/opt/python-3.5.1/bin:${HOME}/.cabal/bin:${HOME}/.local/bin:/usr/local/bin:${PATH}"
 ./build.sh' > job.xml
--- upload-job-config.sh
+++ upload-job-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-~/Code/Personal/jenkins-tools/bin/delete-job.sh jenkins-job-manager
+~/Code/Personal/jenkins-tools/bin/delete-job.sh jenkins-job-manager || true
 ~/Code/Personal/jenkins-tools/bin/put-job.sh jenkins-job-manager job.xml

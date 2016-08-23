83a461f5cea1d3f0afb66569fba13ab84a92f35c
Alexander Reitzel <funtimecoding@gmail.com>
2016-08-23 17:14:46 +0200

    update pylint config

--- generate-job-config.sh
+++ generate-job-config.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
+# shellcheck disable=SC2016
 jjm --locator https://github.com/FunTimeCoding/jenkins-job-manager.git --build-command 'export PATH="${HOME}/opt/python-3.5.1/bin:${PATH}"
 ./build.sh' > job.xml

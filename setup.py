927f25abfef9a5893753abce64b8b172358cd9b1
Alexander Reitzel <funtimecoding@gmail.com>
2017-08-08 22:28:47 +0200

    fill in more setup.py details for testing with twine

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from setuptools import setup
 
 setup(
     name='jenkins-job-manager',
-    version='0.1',
-    description='Stub description for jenkins-job-manager.',
+    version='0.1.0',
+    description='Generate job configuration files for Jenkins.',
+    url='https://github.com/FunTimeCoding/jenkins-job-manager',
+    author='Alexander Reitzel',
+    author_email='funtimecoding@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+    ],
     install_requires=['lxml'],
     scripts=['bin/jjm'],
     packages=['jenkins_job_manager'],
-    author='Alexander Reitzel',
-    author_email='funtimecoding@gmail.com',
-    url='http://example.org',
-    download_url='http://example.org/jenkins-job-manager.tar.gz'
+    download_url='http://funtimecoding.org/jenkins-job-manager.tar.gz',
 )

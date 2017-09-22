c8ca127c0d5bec67da82c67c41f353f3e457cf3e
Alexander Reitzel <funtimecoding@gmail.com>
2017-09-22 18:19:47 +0200

    install python3-wheel to build bdist_wheel, also install tree and git for convenience

--- provision.sh
+++ provision.sh
@@ -6,19 +6,19 @@ CODENAME=$(lsb_release --codename --short)
 if [ "${CODENAME}" = jessie ]; then
     echo Europe/Berlin | tee /etc/timezone
     dpkg-reconfigure --frontend noninteractive tzdata
-    #apt-get --quiet 2 install ntp
-    apt-get --quiet 2 install vim multitail htop
+    apt-get --quiet 2 install vim
 elif [ "${CODENAME}" = stretch ]; then
-    apt-get --quiet 2 install neovim multitail htop
+    apt-get --quiet 2 install neovim
 fi
 
+apt-get --quiet 2 install multitail htop git tree
 echo 'PATH="${HOME}/.gem/ruby/2.3.0/bin:${PATH}"' >> /home/vagrant/.profile
 
 sudo -u vagrant touch /home/vagrant/.pypirc
 chmod 600 /home/vagrant/.pypirc
 cat /vagrant/tmp/pypirc > /home/vagrant/.pypirc
 
-apt-get --quiet 2 install twine ruby-dev build-essential
+apt-get --quiet 2 install twine ruby-dev build-essential python3-wheel
 
 sudo -u vagrant touch /home/vagrant/.gemrc
 chmod 600 /home/vagrant/.gemrc

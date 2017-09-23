4f148cf447d7a61149a2935cf8b4b1a58d2600a4
Alexander Reitzel <funtimecoding@gmail.com>
2017-09-23 22:18:35 +0200

    simplify the install script, drop jessie

--- provision.sh
+++ provision.sh
@@ -1,27 +1,12 @@
 #!/bin/sh -e
 
 export DEBIAN_FRONTEND=noninteractive
-CODENAME=$(lsb_release --codename --short)
-
-if [ "${CODENAME}" = jessie ]; then
-    echo Europe/Berlin | tee /etc/timezone
-    dpkg-reconfigure --frontend noninteractive tzdata
-    apt-get --quiet 2 install vim
-elif [ "${CODENAME}" = stretch ]; then
-    apt-get --quiet 2 install neovim
-fi
-
-apt-get --quiet 2 install multitail htop git tree
+apt-get --quiet 2 install neovim multitail htop git tree twine ruby-dev build-essential python3-wheel python3-venv libyaml-dev libxml2-dev libxslt-dev libenchant-dev hunspell shellcheck
 echo 'PATH="${HOME}/.gem/ruby/2.3.0/bin:${PATH}"' >> /home/vagrant/.profile
-
 sudo -u vagrant touch /home/vagrant/.pypirc
 chmod 600 /home/vagrant/.pypirc
 cat /vagrant/tmp/pypirc > /home/vagrant/.pypirc
-
-apt-get --quiet 2 install twine ruby-dev build-essential python3-wheel python3-venv
-
 sudo -u vagrant touch /home/vagrant/.gemrc
 chmod 600 /home/vagrant/.gemrc
 cat /vagrant/tmp/gemrc > /home/vagrant/.gemrc
-
 sudo -u vagrant gem install fpm

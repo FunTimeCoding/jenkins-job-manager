17e2279c933321f79775d219590e6f3645522cca
Alexander Reitzel <funtimecoding@gmail.com>
2017-09-23 23:01:34 +0200

    make clean.sh obsolete

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -e
 
+if [ ! "${VIRTUAL_ENV}" = "" ]; then
+    # Cannot run the deactivate command here.
+    echo "Virtual environment must not be activated."
+
+    exit 1
+fi
+
+# To guarantee a reproducible build, the virtual environment and build directories must be deleted.
+rm -rf build .venv
+# The Debian package must be deleted before fpm is run.
+rm -f *.deb
+
 ./setup.sh
 # shellcheck source=/dev/null
 . .venv/bin/activate
@@ -7,9 +19,10 @@
 ./style-check.sh --ci-mode
 #./metrics.sh --ci-mode
 ./tests.sh --ci-mode
-./setup.py bdist_wheel
+./setup.py bdist_wheel --dist-dir build
 SYSTEM=$(uname)
 
 if [ "${SYSTEM}" = Linux ]; then
     fpm --input-type python --output-type deb --python-pip /usr/bin/pip3 --python-bin /usr/bin/python3 .
+    mv *.deb build
 fi

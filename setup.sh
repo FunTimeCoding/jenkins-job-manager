80c3ae5ed94cb2abdc901f3372f0b8c60255766e
Alexander Reitzel <funtimecoding@gmail.com>
2017-09-23 15:36:43 +0200

    system dependencies are the responsibility of the configuration management tool

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -e
 
-SYSTEM=$(uname)
-
-if [ "${SYSTEM}" = Darwin ]; then
-    brew install python3 shellcheck
-else
-    sudo apt-get --quiet 2 install shellcheck libenchant-dev hunspell libyaml-dev libxml2-dev libxslt-dev
-fi
-
 if [ ! -d .venv ]; then
     python3 -m venv .venv
 fi

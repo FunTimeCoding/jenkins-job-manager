7eb35f01e3e0ed7fa2bb59f2dcf4c3b0f53b4e95
Alexander Reitzel <funtimecoding@gmail.com>
2017-09-30 21:04:46 +0200

    generate job config with junit, checkstyle and mail recipients

--- upload-job-config.sh
+++ upload-job-config.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
-~/src/jenkins-tools/bin/delete-job.sh jenkins-job-manager || true
-~/src/jenkins-tools/bin/put-job.sh jenkins-job-manager job.xml
+#~/src/jenkins-tools/bin/delete-job.sh jenkins-job-manager || true
+#~/src/jenkins-tools/bin/put-job.sh jenkins-job-manager job.xml
+~/src/jenkins-tools/bin/update-job.sh jenkins-job-manager job.xml

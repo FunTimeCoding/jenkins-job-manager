6968d3f9b4b90d436ffe20260c18e52ea0ee9d98
Alexander Reitzel <funtimecoding@gmail.com>
2017-10-02 17:21:10 +0200

    remove fpm traces since its not used anymore, add pyyaml to provision.sh

--- provision.sh
+++ provision.sh
@@ -1,17 +1,8 @@
 #!/bin/sh -e
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get --quiet 2 install neovim multitail htop git tree twine ruby-dev build-essential devscripts python3-dev python3-venv libyaml-dev libxml2-dev libxslt-dev libenchant-dev hunspell shellcheck python3-all python3-lxml
-
-# shellcheck disable=SC2016
-echo 'PATH="${HOME}/.gem/ruby/2.3.0/bin:${PATH}"' >> /home/vagrant/.profile
+apt-get --quiet 2 install neovim multitail htop git tree twine build-essential devscripts python3-dev python3-venv libyaml-dev libxml2-dev libxslt-dev libenchant-dev hunspell shellcheck python3-all python3-lxml python3-yaml
 
 sudo -u vagrant touch /home/vagrant/.pypirc
 chmod 600 /home/vagrant/.pypirc
 cat /vagrant/tmp/pypirc > /home/vagrant/.pypirc
-
-sudo -u vagrant touch /home/vagrant/.gemrc
-chmod 600 /home/vagrant/.gemrc
-cat /vagrant/tmp/gemrc > /home/vagrant/.gemrc
-
-sudo -u vagrant gem install fpm

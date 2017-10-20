269cdd67f316496925f02323c9da642c7f6f8540
Alexander Reitzel <funtimecoding@gmail.com>
2017-10-20 20:25:41 +0200

    synchronize with skeleton

--- create.sh
+++ create.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -e
 
+mkdir -p tmp
+
+if [ ! -f tmp/ethernet-device.txt ]; then
+    echo eth0 > tmp/ethernet-device.txt
+fi
+
 touch tmp/gemrc
 chmod 600 tmp/gemrc
 cat "${HOME}/.gemrc" > tmp/gemrc

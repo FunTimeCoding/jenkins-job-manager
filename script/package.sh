4cdecbb33c95990ddf1e542d144d7f1504489e13
Alexander Reitzel <funtimecoding@gmail.com>
2018-09-18 16:12:51 +0200

    update version to 0.2.0

--- script/package.sh
+++ script/package.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -e
 
 NAME=jenkins-job-manager
-PROJECT_VERSION=0.1.0
+PROJECT_VERSION=0.2.0
 ARCHIVE="${NAME}_${PROJECT_VERSION}.orig.tar.gz"
 PROJECT_ROOT="${NAME}-${PROJECT_VERSION}"
 PACKAGE_VERSION=1
 COMBINED_VERSION="${PROJECT_VERSION}-${PACKAGE_VERSION}"
 
-if [ ! -f debian/changelog ]; then
-    dch --create -v "${COMBINED_VERSION}" --package "${NAME}"
+if [ -f debian/changelog ]; then
+    dch --newversion "${COMBINED_VERSION}" --package "${NAME}"
+else
+    dch --create --newversion "${COMBINED_VERSION}" --package "${NAME}"
 fi
 
 mkdir -p build

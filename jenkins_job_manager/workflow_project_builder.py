cb16a145edf155e9bd1fd38b36fa9e68fcd4e50d
Alexander Reitzel <funtimecoding@gmail.com>
2019-03-07 14:46:30 +0100

    add test and fix implementation to match the fixture

--- jenkins_job_manager/workflow_project_builder.py
+++ jenkins_job_manager/workflow_project_builder.py
@@ -15,7 +15,7 @@ class WorkflowProjectBuilder(ProjectBuilder):
 
     def __init__(self):
         super().__init__()
-        self.domain = self.join(self.PROJECT_DOMAIN)
+        self.domain = WorkflowProjectBuilder.join(self.PROJECT_DOMAIN)
 
     @staticmethod
     def join(elements: list) -> str:
@@ -29,18 +29,25 @@ class WorkflowProjectBuilder(ProjectBuilder):
 
         parent.append(description)
 
-    @staticmethod
-    def append_folder_views(parent: Element, owner: Element) -> None:
+    def create_owner(self) -> Element:
+        owner = Element('owner')
+        owner.set('class', self.domain)
+        owner.set('reference', '../..')
+
+        return owner
+
+    def append_folder_views(self, parent: Element) -> None:
         folder_views = Element('folderViews')
         folder_views.set('class', 'jenkins.branch.MultiBranchProjectViewHolder')
         folder_views.set('plugin', 'branch-api@2.1.2')
-        folder_views.append(owner)
+        folder_views.append(self.create_owner())
         parent.append(folder_views)
 
-    def append_health_metrics(self, parent: Element) -> None:
+    @staticmethod
+    def append_health_metrics(parent: Element) -> None:
         health_metrics = Element('healthMetrics')
         worst_child_health_metric = Element(
-            self.join(
+            WorkflowProjectBuilder.join(
                 [
                     'com',
                     'cloudbees',
@@ -52,23 +59,38 @@ class WorkflowProjectBuilder(ProjectBuilder):
                 ]
             )
         )
+        worst_child_health_metric.set('plugin', 'cloudbees-folder@6.7')
         non_recursive = Element('nonRecursive')
         non_recursive.text = 'false'
         worst_child_health_metric.append(non_recursive)
         health_metrics.append(worst_child_health_metric)
         parent.append(health_metrics)
 
-    @staticmethod
-    def append_icon(parent: Element, owner: Element) -> None:
+    def append_icon(self, parent: Element) -> None:
         icon = Element('icon')
         icon.set('class', 'jenkins.branch.MetadataActionFolderIcon')
         icon.set('plugin', 'branch-api@2.1.2')
-        icon.append(owner)
+        icon.append(self.create_owner())
         parent.append(icon)
 
     @staticmethod
     def append_orphaned_item_strategy(parent: Element) -> None:
         orphaned_item_strategy = Element('orphanedItemStrategy')
+        orphaned_item_strategy.set(
+            'class',
+            WorkflowProjectBuilder.join(
+                [
+                    'com',
+                    'cloudbees',
+                    'hudson',
+                    'plugins',
+                    'folder',
+                    'computed',
+                    'DefaultOrphanedItemStrategy'
+                ]
+            )
+        )
+        orphaned_item_strategy.set('plugin', 'cloudbees-folder@6.7')
         prune_dead_branches = Element('pruneDeadBranches')
         prune_dead_branches.text = 'true'
         orphaned_item_strategy.append(prune_dead_branches)
@@ -96,19 +118,25 @@ class WorkflowProjectBuilder(ProjectBuilder):
     def append_sources(
             self,
             parent: Element,
-            owner: Element,
     ) -> None:
         sources = Element('sources')
+        sources.set(
+            'class',
+            'jenkins.branch.MultiBranchProject$BranchSourceList'
+        )
+        sources.set('plugin', 'branch-api@2.1.2')
         data = Element('data')
         branch_source = Element('jenkins.branch.BranchSource')
         source = Element('source')
         source.set('class', 'jenkins.plugins.git.GitSCMSource')
         source.set('plugin', 'git@3.9.3')
         identifier = Element('id')
-        # identifier.text = 'Not sure what to put here yet. A UUID?'
+        # Not sure what to put here yet. A UUID?
+        identifier.text = 'example'
         source.append(identifier)
         remote = Element('remote')
         remote.text = self.repository_locator
+        source.append(remote)
         credentials_identifier = Element('credentialsId')
         source.append(credentials_identifier)
         traits = Element('traits')
@@ -122,29 +150,53 @@ class WorkflowProjectBuilder(ProjectBuilder):
         properties = Element('properties')
         properties.set('class', 'empty-list')
         strategy.append(properties)
+        branch_source.append(source)
+        branch_source.append(strategy)
         data.append(branch_source)
         sources.append(data)
-        sources.append(owner)
+        sources.append(self.create_owner())
 
         parent.append(sources)
 
+    def append_factory(
+            self,
+            parent: Element,
+    ) -> None:
+        factory = Element('factory')
+        factory.set(
+            'class',
+            WorkflowProjectBuilder.join(
+                [
+                    'org',
+                    'jenkinsci',
+                    'plugins',
+                    'workflow',
+                    'multibranch',
+                    'WorkflowBranchProjectFactory'
+                ]
+            )
+        )
+        factory.append(self.create_owner())
+        script_path = Element('scriptPath')
+        script_path.text = 'Jenkinsfile'
+        factory.append(script_path)
+        parent.append(factory)
+
     def build(self) -> Element:
         project = Element(self.domain)
+        project.set('plugin', 'workflow-multibranch@2.20')
 
         project.append(Element('actions'))
         self.append_description(project)
         project.append(Element('properties'))
 
-        owner = Element('owner')
-        owner.set('class', self.domain)
-        owner.set('reference', '../..')
-
-        self.append_folder_views(project, owner)
+        self.append_folder_views(project)
         self.append_health_metrics(project)
-        self.append_icon(project, owner)
+        self.append_icon(project)
         self.append_orphaned_item_strategy(project)
         project.append(Element('triggers'))
         self.append_disabled(project)
-        self.append_sources(project, owner)
+        self.append_sources(project)
+        self.append_factory(project)
 
         return project

f863e4aefadb32e6858a968431bb73ee0154b148
Alexander Reitzel <funtimecoding@gmail.com>
2020-06-09 17:39:45 +0200

    allow shortening all tags

--- jenkins_job_manager/lxml_helper.py
+++ jenkins_job_manager/lxml_helper.py
@@ -3,24 +3,6 @@ from lxml.etree import Element
 
 
 def serialize_element(element: Element) -> str:
-    tags_to_not_shorten = [
-        'description',
-        'id',
-        'name',
-        'reportEncoding',
-        'sourceCodeEncoding',
-        'sourceDirectory',
-        'referenceJobName',
-        'sourceExclusionPattern',
-        'inclusionPattern',
-        'exclusionPattern',
-    ]
-
-    for node in element.iter():
-        if node.tag in tags_to_not_shorten:
-            if node.text is None:
-                node.text = ''
-
     return etree.tostring(
         element_or_tree=element,
         encoding='unicode',
